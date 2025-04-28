615567046dc26cd21cba0b8556e32301b3581c2f
gtkarthikeyan26 <gtrkarthikeyan@gmail.com>
2025-04-28 12:30:42 +0530

    changes

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 const app = express();
 app.use(express.json()); // middleware to parse json
-
+app.get('/', (req, res) => {
+    res.send('Product Store API is running');
+  });
 app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-
+connectDB();
 app.listen(PORT, () => {
-    connectDB();
+    
     console.log(`Server is running on port ${PORT} hello`);
 });
 
